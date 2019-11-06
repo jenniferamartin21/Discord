0235f2208c1965fc34e7e90a41b1a415161dbdea
jenniferamartin21 <48300490+jenniferamartin21@users.noreply.github.com>
2019-11-06 11:17:52 -0600

    update bot.py

--- bot.py
+++ bot.py
@@ -1,45 +1,66 @@
 import discord
+# from dotenv import load_dotenv
+
+import os
+
 #from discord.ext.commands import Bot
 
 # @bot.command()
 # async def 
-TOKEN = 'NjI1ODYyMjU0MDc1ODM4NDc0.XcIxZQ.OGO4u6HjmlEimqXLvHopKk78_kg'
+#load_dotenv()
+token = 'NjI1ODYyMjU0MDc1ODM4NDc0.XcIxZQ.OGO4u6HjmlEimqXLvHopKk78_kg'
 # client = Bot()
-
 client = discord.Client()
+
+'''
+class CustomClient(discord.Client):
+    async def on_ready(self):
+        print(f'{self.user} is connected to Discord')
+client = CustomClient()
+'''
 '''
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     if message.content.startswith('I\'m'):
-        mssg = 'Hello {message.conent}'.format(message)
-        await client.send_message(message.channel,mssg)
+        msg = 'Hello {}'.format(message.content)
+        await client.send_message(message.channel,msg)
 '''
 '''
 @client.command(name='I\'m',aliases='I am',pass_context=True)
 async def joke(context):
     await message.content('Hi, ' + context.message + 'I\'m dad.')
 '''
+
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+    print('Bot is ready')
+    # print(client.user.name)
+    # print(client.user.id)
+    # print('------')
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
-        pass
-
+        return
+    
+    if 'I\'m' in message.content:
+        msg = message.content
+        msg = msg.replace('I\'m', '')
+        dad_joke = 'Hello {}, I\'m dad.'.format(msg)
+        await message.channel.send(dad_joke)
+        await client.process_commands(msg)
+'''
 def dad_joke(message, client):
     return 'Hello {}, I\'m dad.'.format(message)
 dad_joke_command = {
     'trigger': 'I\'m',
     'function': dad_joke,
 }
+'''
 
 
 
-client.run(TOKEN)
+client.run(token)
